d890b70e0228407f27406f43b3cdf2424a55095a
Matt Stauffer <matt@tighten.co>
2018-08-24 09:34:13 -0400

    Clean up and pull page view metrics back

---
+++ src/PageViewsMetric.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tightenco\NovaGoogleAnalytics;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Laravel\Nova\Metrics\Value;
+use Spatie\Analytics\Analytics;
+
+class PageViewsMetric extends Value
+{
+    public $name = 'GA Page Views Today';
+
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function calculate(Request $request)
+    {
+        return $this
+            ->result($this->pageViewsForDay(now()))
+            ->previous($this->pageViewsForDay(now()->subDay()));
+    }
+
+    private function pageViewsForDay(Carbon $day)
+    {
+        // $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(2));
+
+        // @todo: Extract last two days' page view data from ^^
+
+        if ($day->isToday()) {
+            return 110;
+        }
+
+        return 42;
+    }
+
+    /**
+     * Get the ranges available for the metric.
+     *
+     * @return array
+     */
+    public function ranges()
+    {
+        return [
+            // 1 => '1 day',
+            // 30 => '30 Days',
+            // 60 => '60 Days',
+            // 365 => '365 Days',
+            // 'MTD' => 'Month To Date',
+            // 'QTD' => 'Quarter To Date',
+            // 'YTD' => 'Year To Date',
+        ];
+    }
+
+    /**
+     * Determine for how many minutes the metric should be cached.
+     *
+     * @return  \DateTimeInterface|\DateInterval|float|int
+     */
+    public function cacheFor()
+    {
+        // return now()->addMinutes(5);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'page-views';
+    }
+}
