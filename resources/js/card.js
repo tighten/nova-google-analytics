f958458616ecda2a38a83d8cfbf6b757bd5ff94f
Matt Stauffer <matt@tighten.co>
2018-08-27 13:27:27 -0400

    Take first steps toward a most visited pages card

---
+++ resources/js/card.js
@@ -0,0 +1,3 @@
+Nova.booting((Vue, router) => {
+    Vue.component('most-visited-pages', require('./components/MostVisitedPages'));
+})
---
+++ resources/js/components/MostVisitedPages.vue
@@ -0,0 +1,17 @@
+<template>
+    <card class="flex flex-col items-center justify-center">
+        <div class="px-3 py-3">
+            <h1 class="text-center text-3xl text-80 font-light">Most Visited Card!</h1>
+        </div>
+    </card>
+</template>
+
+<script>
+export default {
+    props: ['card'],
+
+    mounted() {
+        console.log("Mounted");
+    },
+}
+</script>
