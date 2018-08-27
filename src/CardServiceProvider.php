bb98e8dd6be777f27be29b5576cfafa06d49db3f
Matt Stauffer <matt@tighten.co>
2018-08-27 13:53:14 -0400

    Add CardServiceProvider

---
+++ src/CardServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tightenco\NovaGoogleAnalytics;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class CardServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('nova-google-analytics', __DIR__.'/../dist/js/card.js');
+            Nova::style('nova-google-analytics', __DIR__.'/../dist/css/card.css');
+        });
+    }
+
+    /**
+     * Register the card's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+        Route::middleware(['nova'])
+                ->prefix('nova-vendor/nova-google-analytics')
+                ->group(__DIR__.'/../routes/api.php');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
