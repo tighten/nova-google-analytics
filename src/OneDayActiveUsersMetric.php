691f367ed6752f99bd39f10d2ba0ba9ca70e6015
Kristin Collins <kristin@tighten.co>
2021-06-17 10:49:42 -0400

    Add Trend for 1 Day Active Users.

---
+++ src/OneDayActiveUsersMetric.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Tightenco\NovaGoogleAnalytics;
+
+use Carbon\Traits\Creator;
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use Laravel\Nova\Metrics\Trend;
+use Laravel\Nova\Metrics\TrendResult;
+use Spatie\Analytics\Analytics;
+use Spatie\Analytics\Period;
+use Carbon\Carbon;
+
+class OneDayActiveUsersMetric extends Trend
+{
+    public function name() {
+        return __('1 Day Active Users');
+    }
+
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function calculate(Request $request)
+    {
+        $lookups = [
+            5 => $this->activeUsersFiveDays(),
+            10 => $this->activeUsersTenDays(),
+            15 => $this->activeUsersFifteenDays(),
+        ];
+
+        $data = Arr::get($lookups, $request->get('range'), ['results' => [0]]);
+
+        return (new TrendResult)->trend($data['results'])
+            ->showLatestValue()
+            ->format('0,0');
+    }
+
+    private function activeUsersFiveDays()
+    {
+        $analyticsData = app(Analytics::class)
+            ->performQuery(
+                Period::days(5),
+                'ga:1dayUsers',
+                [
+                    'metrics' => 'ga:1dayUsers',
+                    'dimensions' => 'ga:date',
+                ]
+            );
+
+        $rows = collect($analyticsData->getRows());
+
+        $results = [];
+        foreach ($rows as $row) {
+            $date = new Carbon($row[0]);
+            $results[$date->format('M j')] = intval($row[1]);
+        }
+
+        return ['results' => $results];
+    }
+
+    private function activeUsersTenDays()
+    {
+        $analyticsData = app(Analytics::class)
+            ->performQuery(
+                Period::days(10),
+                'ga:1dayUsers',
+                [
+                    'metrics' => 'ga:1dayUsers',
+                    'dimensions' => 'ga:date',
+                ]
+            );
+
+        $rows = collect($analyticsData->getRows());
+
+        $results = [];
+        foreach ($rows as $row) {
+            $date = new Carbon($row[0]);
+            $results[$date->format('M j')] = intval($row[1]);
+        }
+
+        return ['results' => $results];
+    }
+
+    private function activeUsersFifteenDays()
+    {
+        $analyticsData = app(Analytics::class)
+            ->performQuery(
+                Period::days(15),
+                'ga:1dayUsers',
+                [
+                    'metrics' => 'ga:1dayUsers',
+                    'dimensions' => 'ga:date',
+                ]
+            );
+
+        $rows = collect($analyticsData->getRows());
+
+        $results = [];
+        foreach ($rows as $row) {
+            $date = new Carbon($row[0]);
+            $results[$date->format('M j')] = intval($row[1]);
+        }
+
+        return ['results' => $results];
+    }
+
+    /**
+     * Get the ranges available for the metric.
+     *
+     * @return array
+     */
+    public function ranges()
+    {
+        return [
+            5 => '5 Days',
+            10 => '10 Days',
+            15 => '15 Days',
+        ];
+    }
+
+    /**
+     * Determine for how many minutes the metric should be cached.
+     *
+     * @return  \DateTimeInterface|\DateInterval|float|int
+     */
+    public function cacheFor()
+    {
+        //return now()->addMinutes(30);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'one-day-active-users';
+    }
+}
